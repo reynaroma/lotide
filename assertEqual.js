529ef4720dfb60efe349f02d36ddd6a70053e9e2
Reyna May <roma.reynamayA@gmail.com>
2023-06-26 21:38:50 -0600

    Successfully created my own assert

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const checkMark = '\u2705';
+const redMark = '\u{1F6D1}';
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(checkMark + checkMark + checkMark + `Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.error(redMark + redMark + redMark + `Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('hello', 'hello');
+assertEqual('hi', 'hello');
+assertEqual(2, 2);
+assertEqual(1, 2);
