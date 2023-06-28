16ec98a94fb01bd9efa4ceb4955a3ecafe189832
Reyna May <roma.reynamayA@gmail.com>
2023-06-27 20:32:26 -0600

    Created a function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const checkMark = '\u2705';
+const redMark = '\u{1F6D1}';
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`${checkMark}${checkMark}${checkMark}Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.error(`${redMark}${redMark}${redMark}Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  // check if the lengths of array are equal
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  // Iterate over the elements of the arrays
+  for (let i = 0; i < arr1.length; i++) {
+    // check if the the current elements are not equal
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
