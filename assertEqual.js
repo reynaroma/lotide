8c578006005531a2fd40680de0e7fc3605c2c996
Reyna May <roma.reynamayA@gmail.com>
2023-07-03 15:44:10 -0600

    Refactor the assertArray and tail for cleanup

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,12 @@
 const assertEqual = function(actual, expected) {
   const checkMark = '\u2705';
   const redMark = '\u{1F6D1}';
-  
+  // Helper function to compare arrays for equality
+  if (Array.isArray(actual) && Array.isArray(expected)) {
+    actual = JSON.stringify(actual);
+    expected = JSON.stringify(expected);
+  }
+  // AssertEqual own
   if (actual === expected) {
     console.log(`${checkMark}${checkMark}${checkMark}Assertion Passed: ${actual} === ${expected}`);
   } else {
--- tail.js
+++ tail.js
@@ -1,6 +1,6 @@
-const checkMark = '\u2705';
-const redMark = '\u{1F6D1}';
-const assertEqual = function(actual, expected) {
+/*const assertEqual = function(actual, expected) {
+  const checkMark = '\u2705';
+  const redMark = '\u{1F6D1}';
   // Helper function to compare arrays for equality
   if (Array.isArray(actual) && Array.isArray(expected)) {
     actual = JSON.stringify(actual);
@@ -12,17 +12,16 @@ const assertEqual = function(actual, expected) {
   } else {
     console.error(`${redMark}${redMark}${redMark}Assertion Failed: ${actual} !== ${expected}`);
   }
-};
-
-let arr = [];
+};*/
 
 const tail = function(array) {
+  let arr = [];
+
   arr = array.slice(1);
+
   return arr;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+// assertEqual(["Yo Yo", "Lighthouse", "Labs"].length, 3); // original array should still have 3 elements!
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+module.exports = tail;
