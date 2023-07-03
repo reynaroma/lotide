9dcfa62884fcf6e0972337d1c1208ea5c0b7f28c
Reyna May <roma.reynamayA@gmail.com>
2023-07-03 15:19:00 -0600

    Clean up the main program and copied the test code from their respective program

---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+// test/assertEqualtest.js
+// path/directory of the file
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('hello', 'hello');
+assertEqual('hi', 'hello');
+assertEqual(2, 2);
+assertEqual(1, 2);
