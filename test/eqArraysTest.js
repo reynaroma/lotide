243351f6ff7037f514312de7a7b4fbad1b256046
Reyna May <roma.reynamayA@gmail.com>
2023-07-03 15:55:43 -0600

    Separate the TEST code of eqArrays.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+// path/directory of the file
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
