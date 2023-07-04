da996a672c797cafc6414a436dcd5f3ea39ab676
Reyna May <roma.reynamayA@gmail.com>
2023-07-03 18:21:59 -0600

    Implement the mocha & chai instead of using my assertish functions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,26 @@
-//path/directory
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]()
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [1, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), []);
+describe("#middle", () => {
+  it("returns [] when array is [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] when array is [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns [2] when array is [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [3] when array is [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [2, 4] when array is [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3, 4] when array is [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("returns [0, 4] when array is [8, 2, 3, 0, 4, 5, 9, 0]", () => {
+    assert.deepEqual(middle([8, 2, 3, 0, 4, 5, 9, 0]), [0, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,17 @@
-// path/directory of the file
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-// TEST CODE
-assertEqual(["Yo Yo", "Lighthouse", "Labs"].length, 3); // original array should still have 3 elements!
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-assertEqual(tail(["apple", "orange", "strawberry"]), ["orange", "strawberry"]);
-assertEqual(tail([1, 2, 5, 7]), [2, 5, 7]);
-assertEqual(tail([7, 'a', 5, '2']), ['a', 5, 21]);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns [1, 2, 5, 7]", () => {
+    assert.deepEqual(tail([1, 2, 5, 7]), [2, 5, 7]);
+  });
+  it("returns [7, 'a', 5, '2']", () => {
+    assert.deepEqual(tail([7, 'a', 5, '2']), ['a', 5, '2']);
+  });
+  it("returns ['Reyna', 'Lighthouse', 'Roma, 'Labs']", () => {
+    assert.deepEqual(tail([42, 'Reyna', 'Lighthouse', 'Roma', 'Labs']), ['Reyna', 'Lighthouse', 'Roma', 'Labs']);
+  });
+});
