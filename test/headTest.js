f8356e8c5bd4d9f330563f66c44cc83cc2253579
Reyna May <roma.reynamayA@gmail.com>
2023-07-03 18:00:19 -0600

    Implemented M&C for real testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,19 @@
 // headTest.js
-// path/directory of the file
-const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assert = require('chai').assert;
+const head = require('../head');
+
+describe('#head', () => {
+  it("returns '1' for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for [5,6,7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 'A' for ['A', '3', 3, 'B']", () => {
+    assert.strictEqual(head(['A', '3', 3, 'B']), 'A');
+  });
+});
